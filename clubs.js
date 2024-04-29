82c38ba246a8150af8c709e2c65fe1f9e315b095
Jacob Lopez <jacobrlopez22@berkeley.edu>
2024-04-29 14:47:35 -0700

    Jacob's Recent Combination w/ Liam

--- clubs.js
+++ clubs.js
@@ -1,4 +1,11 @@
-// Sample data
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar__menu');
+
+menu.addEventListener('click', function() {
+  menu.classList.toggle('is-active');
+  menuLinks.classList.toggle('active');
+});
+
 const imageData = [
     { title: "Web Development Berkeley", description: "Web Development @ Berkeley (WDB) is UC Berkeley’s Premier Web Design and Development organization. They are a group of students focusing on building the web development and design community.", imageUrl: "images/WDB.png", linkUrl: "https://www.webatberkeley.org/" },
     { title: "Codebase", description: "Codebase is a community of developers that empowers students to break into the software industry.", imageUrl: "images/Codebase.png", linkUrl: "https://codebase.studentorg.berkeley.edu/" },
@@ -8,11 +15,8 @@ const imageData = [
     { title: "SUPERB", description: "Founded in 1964, they are dedicated to providing entertainment for the campus and community through the work of our nine departments: General Management, Concerts, Games, Films, Sneak Previews, Comedy, Marketing, Photography, and Art & Design. Our 100-person production team is composed entirely of undergraduate students who are passionate about bringing great entertainment to the UC Berkeley campus and community.", imageUrl: "images/Superb.png", linkUrl: "https://www.superb.berkeley.edu/" },
     { title: "Berkeley Consulting", description: "For over 28 years, Berkeley Consulting has been an advisor to a broad range of organizations, from Fortune 500 companies to start-ups and non-profits. Started in 1996 with the mission of gathering the brightest minds on campus and putting them to use solving pressing real-world issues, Berkeley Consulting has grown to be a trusted partner for some of the biggest names across industry. They have developed an experience that cultivates not only our members’ consulting abilities but a career-agnostic understanding of problem-solving that they carry with them into tech, banking, and public service.", imageUrl: "images/BerkeleyConsulting.png", linkUrl: "https://bc.studentorg.berkeley.edu/" },
     { title: "Voyager Consulting", description: "Voyager Consulting is a student-run, business organization that provides strategy consulting services to growth stage and Fortune 500 companies. Based in UC Berkeley, our consultants hail from diverse backgrounds and go through a rigorous selection and training process in order to deliver the best quality work to our clients.", imageUrl: "images/Voyager.png", linkUrl: "https://www.voyagerconsulting.org/" },
-
-    // Add more image data as needed
 ];
 
-// Function to dynamically create grid cells
 function createGridCells() {
     const gridContainer = document.querySelector('.grid');
     imageData.forEach(data => {
@@ -39,5 +43,4 @@ function createGridCells() {
     });
 }
 
-// Call the function to create grid cells when the page loads
 window.onload = createGridCells;
